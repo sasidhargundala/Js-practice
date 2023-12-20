6ec23b09e3fb5ad4f26bd3871850a26bc38da207
sasidhargundala <117711272+sasidhargundala@users.noreply.github.com>
2023-12-20 07:30:06 +0530

    worked on 19-12-2023 JS Varibles and Data Type

---
+++ hello.js
@@ -0,0 +1,13 @@
+console.log("helloworld")
+let num=10
+let num2=20
+
+console.log(num+num2)
+console.log(num-num2)
+console.log(num*num2)
+console.log(num/num2)
+var num1=20
+var num3=50
+var username="sasi"
+console.log(num1+num3)
+console.log(username)
